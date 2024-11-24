59ad8890c8f264dd695c981e51a2ea8e085c5a13
Aman Kumar <aman.kumar2k15@gmail.com>
2024-11-24 21:49:33 +0530

    created route , login page and signup page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,22 @@
-import { LoginForm } from "./components/auth/LoginForm";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 function App() {
-  return <LoginForm />;
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<SignUp />} />
+      </Routes>
+    </ThemeProvider>
+  );
 }
 export default App;
--- client/src/components/auth/SignUpForm.tsx
+++ client/src/components/auth/SignUpForm.tsx
@@ -1,14 +1,14 @@
-import { TextField, Typography, Button } from "@mui/material";
+import { TextField, Button, Typography } from "@mui/material";
 import React from "react";
 import { Link } from "react-router-dom";
 
-export const LoginForm: React.FC = () => {
+export const SignUpForm: React.FC = () => {
   const [email, setEmail] = React.useState("");
   const [emailErrored, setEmailErrored] = React.useState(false);
   const [passwordErrored, setPasswordErrored] = React.useState(false);
   const [password, setPassword] = React.useState("");
 
-  const handleLogin = (e) => {
+  const handleRegister = (e) => {
     e.preventDefault();
     !email ? setEmailErrored(true) : setEmailErrored(false);
     !password ? setPasswordErrored(true) : setPasswordErrored(false);
@@ -38,12 +38,12 @@ export const LoginForm: React.FC = () => {
           onChange={(event) => setPassword(event.target.value)}
           error={passwordErrored}
         />
-        <Link to="/register" className="justify-self-start self-start mt-2">
-          <Typography className="underline">Sign Up</Typography>
+        <Link to="/login" className="justify-self-start self-start mt-2">
+          <Typography className="underline">Login</Typography>
         </Link>
       </div>
-      <Button variant="contained" className="w-80" onClick={handleLogin}>
-        <span className="p-1">Login</span>
+      <Button variant="contained" className="w-80" onClick={handleRegister}>
+        <span className="p-1">Register</span>
       </Button>
     </div>
   );
